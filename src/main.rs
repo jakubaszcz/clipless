3b47b23648f82e0a5e16989ff16656e84321a3e2
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-04-07 22:59:52 +0200

    feat: create loop for keybind listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use global_hotkey::{GlobalHotKeyEvent, GlobalHotKeyManager, hotkey::{HotKey, Modifiers, Code}};
+use winit::event_loop::EventLoop;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    let manager = GlobalHotKeyManager::new().unwrap();
+
+    let custom_copy_hotkey = HotKey::new(
+        Some(Modifiers::CONTROL | Modifiers::ALT),
+        Code::KeyC,
+    );
+
+    manager.register(custom_copy_hotkey).unwrap();
+
+    let receiver = GlobalHotKeyEvent::receiver();
+
+    event_loop.run(move |_event, _| {
+        if let Ok(_event) = receiver.try_recv() {
+            println!("{:?}", _event);
+        }
+    }).unwrap();
 }
