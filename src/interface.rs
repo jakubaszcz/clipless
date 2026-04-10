0cd699a671cbecbc72af241d5b27e8c63017a7aa
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-04-10 23:25:22 +0200

    feat: add interface styling

--- src/interface.rs
+++ src/interface.rs
@@ -52,35 +52,73 @@ fn clip_box(ui: &mut egui::Ui, ctx : &Context, clip: clipboard::Clipboard, conne
         clip.content.to_string()
     };
 
-    egui::Frame::group(ui.style())
-        .inner_margin(egui::Margin::same(6))
+    egui::Frame::new()
+        .fill(egui::Color32::from_hex("#F2EFE9").unwrap())
+        .corner_radius(2.0)
         .show(ui, |ui| {
-            ui.vertical(|ui| {
-                ui.set_min_width(ui.available_width());
+            egui::Frame::new()
+                .inner_margin(egui::Margin::same(6))
+                .show(ui, |ui| {
+                    ui.vertical(|ui| {
+                        ui.set_min_width(ui.available_width());
 
-                ui.label(display_text);
-                ui.horizontal(|ui| {
-                    if ui.button("Copy").clicked() {
-                        ui.ctx().copy_text(clip.content.to_string());
-                        database::update_use_clip(connection, clip.id).unwrap();
-                    }
-                    if ui.button("Delete").clicked() {
-                        database::remove_clip(&connection, clip.id).unwrap();
-                    }
+                        ui.label(
+                            egui::RichText::new(display_text)
+                                .color(egui::Color32::from_hex("#252627").unwrap())
+                        );
+                        ui.horizontal(|ui| {
+                            if ui.add(
+                                egui::Button::new(
+                                    egui::RichText::new("Copy")
+                                        .color(egui::Color32::from_hex("#F2EFE9").unwrap())
+                                        .strong()
+                                )
+                                    .fill(egui::Color32::from_hex("#252627").unwrap())
+                                    .corner_radius(egui::CornerRadius::same(2))
+                            ).clicked() {
+                                ui.ctx().copy_text(clip.content.to_string());
+                                database::update_use_clip(connection, clip.id).unwrap();
+                            }
+                            if ui.add(
+                                egui::Button::new(
+                                    egui::RichText::new("Delete")
+                                        .color(egui::Color32::from_hex("#F2EFE9").unwrap())
+                                        .strong()
+                                )
+                                    .fill(egui::Color32::from_hex("#252627").unwrap())
+                                    .corner_radius(egui::CornerRadius::same(2))
+                            ).clicked() {
+                                database::remove_clip(connection, clip.id).unwrap();
+                            }
+                            if clip.content.chars().count() > max_text_length {
+                                if ui.add(
+                                    egui::Button::new(
+                                        egui::RichText::new("Expand")
+                                            .color(egui::Color32::from_hex("#F2EFE9").unwrap())
+                                            .strong()
+                                    )
+                                        .fill(egui::Color32::from_hex("#252627").unwrap())
+                                        .corner_radius(egui::CornerRadius::same(2))
+                                ).clicked() {
+                                    *clip_modal = Some(clip.id);
+                                }
+                            }
 
-                    if clip.content.chars().count() > max_text_length {
-                        if ui.button("Expand").clicked() {
-                            *clip_modal = Some(clip.id)
-                        }
-                    }
-                    ui.label(DateTime::<Utc>::from_timestamp(clip.timestamp, 0).unwrap().format("%Y-%m-%d %H:%M:%S").to_string());
-                    if clip.use_clip > 0 {
-                        ui.separator();
-                        ui.label(format!("It has been copied {} times", clip.use_clip));
-                    }
+                            ui.label(
+                                egui::RichText::new(DateTime::<Utc>::from_timestamp(clip.timestamp, 0).unwrap().format("%Y-%m-%d %H:%M:%S").to_string())
+                                    .color(egui::Color32::from_hex("#252627").unwrap())
+                            );
+                            if clip.use_clip > 0 {
+                                ui.add_space(10.0);
+                                ui.label(
+                                    egui::RichText::new(format!("It has been copied {} times", clip.use_clip))
+                                        .color(egui::Color32::from_hex("#252627").unwrap())
+                                );
+                            }
+                        })
                 })
-            });
         });
+    });
 
     // Modal box
     if *clip_modal == Some(clip.id) {
@@ -91,21 +129,48 @@ fn clip_box(ui: &mut egui::Ui, ctx : &Context, clip: clipboard::Clipboard, conne
 // Define the modal box function
 fn modal_box(ctx: &Context, clip: clipboard::Clipboard, connection: &Connection ,clip_modal: &mut Option<u32>) {
     if egui::Modal::new(egui::Id::new("my_modal"))
+        .frame(
+            egui::Frame::new()
+                .corner_radius(2.0)
+                .inner_margin(egui::Margin::same(6))
+                .fill(egui::Color32::from_hex("#F2EFE9").unwrap()))
         .show(ctx, |ui| {
             egui::ScrollArea::vertical().show(ui, |ui| {
                 ui.label(clip.content.to_string());
                 ui.horizontal(|ui| {
-                    if ui.button("Copy").clicked() {
+                    if ui.add(
+                        egui::Button::new(
+                            egui::RichText::new("Copy")
+                                .color(egui::Color32::from_hex("#F2EFE9").unwrap())
+                                .strong()
+                        )
+                            .fill(egui::Color32::from_hex("#252627").unwrap())
+                            .corner_radius(egui::CornerRadius::same(2))
+                    ).clicked() {
                         ui.ctx().copy_text(clip.content.to_string());
                         database::update_use_clip(connection, clip.id).unwrap();
                     }
-                    if ui.button("Delete").clicked() {
-                        database::remove_clip(&connection, clip.id).unwrap();
+                    if ui.add(
+                        egui::Button::new(
+                            egui::RichText::new("Delete")
+                                .color(egui::Color32::from_hex("#F2EFE9").unwrap())
+                                .strong()
+                        )
+                            .fill(egui::Color32::from_hex("#252627").unwrap())
+                            .corner_radius(egui::CornerRadius::same(2))
+                    ).clicked() {
+                        database::remove_clip(connection, clip.id).unwrap();
                     }
-                    ui.label(DateTime::<Utc>::from_timestamp(clip.timestamp, 0).unwrap().format("%Y-%m-%d %H:%M:%S").to_string());
+                    ui.label(
+                        egui::RichText::new(DateTime::<Utc>::from_timestamp(clip.timestamp, 0).unwrap().format("%Y-%m-%d %H:%M:%S").to_string())
+                            .color(egui::Color32::from_hex("#252627").unwrap())
+                    );
                     if clip.use_clip > 0 {
-                        ui.separator();
-                        ui.label(format!("It has been copied {} times", clip.use_clip));
+                        ui.add_space(10.0);
+                        ui.label(
+                            egui::RichText::new(format!("It has been copied {} times", clip.use_clip))
+                                .color(egui::Color32::from_hex("#252627").unwrap())
+                        );
                     }
                 })
             });
@@ -120,35 +185,69 @@ impl App for MyApp {
 
         ctx.send_viewport_cmd(ViewportCommand::Minimized(false));
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Store Clipboard with CTRL + LALT + C");
-            ui.label("Press CTRL + LALT + D to focus the window, do not close the app, if it happen it will shut down. Just turn the executable back.");
-            ui.horizontal(|ui| {
-                ui.add(
-                    egui::TextEdit::singleline(&mut self.search_query).hint_text("Search up a clip...")
-                );
-            });
-            egui::ScrollArea::vertical().show(ui, |ui| {
+        egui::CentralPanel::default()
+            .frame(
+                egui::Frame::new()
+                    .fill(egui::Color32::from_hex("#252627").unwrap())
+            )
+            .show(ctx, |ui| {
 
-                let query: Vec<clipboard::Clipboard>;
+                // Header
+                egui::Frame::new()
+                    .inner_margin(egui::Margin::same(6))
+                    .show(ui, |ui| {
+                        ui.horizontal(|ui| {
+                            ui.label(
+                                egui::RichText::new("Clipless")
+                                    .size(20.0)
+                                    .color(egui::Color32::from_hex("#F2EFE9").unwrap())
+                            );
+                            ui.add_space(10.0);
+                            ui.add(
+                                egui::TextEdit::singleline(&mut self.search_query).hint_text("Search up a clip...")
+                                    .background_color(egui::Color32::from_hex("#F2EFE9").unwrap())
+                                    .text_color(egui::Color32::from_hex("#252627").unwrap())
+                                    .lock_focus(false)
+                            );
+                        })
+                    });
 
-                // If the query is longer than 3 characters, fetch the clips from the database, otherwise get all clips
-                if self.search_query.len() > 3 {
-                    query = database::fetch_clips(&self.connection, &self.search_query).unwrap();
-                } else {
-                    query = database::get_clips(&self.connection).unwrap();
-                }
+                // Content
+                egui::Frame::new()
+                    .inner_margin(egui::Margin::same(6))
+                    .show(ui, |ui| {
+                        egui::ScrollArea::vertical().show(ui, |ui| {
 
-                // If there are no clips, display a message
-                if query.is_empty() {
-                    ui.label("No clips found...");
-                }
+                            let query: Vec<clipboard::Clipboard>;
 
-                // Display the clips
-                for clips in query {
-                    clip_box(ui, ctx, clips, &self.connection, &mut self.clip_modal);
-                }
-            })
+                            // If the query is longer than 3 characters, fetch the clips from the database, otherwise get all clips
+                            if self.search_query.len() > 3 {
+                                query = database::fetch_clips(&self.connection, &self.search_query).unwrap();
+                            } else {
+                                query = database::get_clips(&self.connection).unwrap();
+                            }
+
+                            // If there are no clips, display a message
+                            let empty = egui::Frame::new();
+
+                            if query.is_empty() {
+                                empty.show(ui, |ui| {
+                                    ui.centered_and_justified(|ui| {
+                                        ui.label(
+                                            egui::RichText::new("Nothing to be found...")
+                                                .color(egui::Color32::from_hex("#F2EFE9").unwrap())
+                                        );
+                                    })
+                                });
+                                return;
+                            }
+
+                            // Display the clips
+                            for clips in query {
+                                clip_box(ui, ctx, clips, &self.connection, &mut self.clip_modal);
+                            }
+                        })
+                    });
         });
 
         ctx.request_repaint();
